54609db59781edb2b3e73524809e8305f587492a
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-10-21 10:52:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let x_value = 0
+let y_value = 0
+input.onButtonPressed(Button.A, function () {
+    while (x_value <= 4) {
+        led.plot(x_value, 0)
+        x_value += 1
+        basic.pause(200)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (y_value <= 4) {
+        led.plot(0, y_value)
+        y_value += 1
+        basic.pause(200)
+        basic.clearScreen()
+    }
+})
+basic.forever(function () {
+	
+})
