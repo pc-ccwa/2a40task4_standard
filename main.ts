9019b40ea15e16acc54a56ba3654579a88db4ece
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-10-27 09:48:22 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let x_value = 0
 let y_value = 0
 input.onButtonPressed(Button.A, function () {
+    x_value = 0
     while (x_value <= 4) {
         led.plot(x_value, 0)
         x_value += 1
@@ -9,6 +10,7 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
+    y_value = 0
     while (y_value <= 4) {
         led.plot(0, y_value)
         y_value += 1
